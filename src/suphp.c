0c2a04bd400b98b9590cd9cff8119586ce32c8fe
Simon Uyttendaele <simon@anotherservice.com>
2012-01-26 23:52:51 +0100

    Under license

--- src/suphp.c
+++ src/suphp.c
@@ -3,7 +3,38 @@
   | PHP Version 5 - Copyright (c) 1997-2007 The PHP Group               |
   +---------------------------------------------------------------------+
   | Author: Simon Uyttendaele                                           |
+  | Parts of this source file may be subject to version 3.01 of the PHP |
+  | license. http://www.php.net/license/3_01.txt                        |
+  |                                                                     |
+  | Uncovered parts from the above license are subject to the FreeBSD   |
+  | license below with respective copyrights.                           |
   +---------------------------------------------------------------------+
+ 
+Copyright (c) 2012, Simon Uyttendaele <simon@olympe-network.com>
+Granted Copyright (c) 2012, Olympe Network <contact@olympe-network.com>
+Granted Copyright (c) 2012, SYS S.A.S. <contact@anotherservice.com>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /* $Id: header,v 1.16.2.1.2.1 2007/01/01 19:32:09 iliaa Exp $ */
